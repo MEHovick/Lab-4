86a186c86b03970a6152e1416a7e0d66445a7cc9
Dramb1 <63962317+Dramb1@users.noreply.github.com>
2021-10-20 12:28:55 +0500

    Create function man

---
+++ man.cpp
@@ -0,0 +1,11 @@
+#include "graphics.h"
+#include "picture.hpp"
+
+void man() {
+   setcolor(RED);
+   circle(50, 50, 10);
+   line(50, 60, 50, 80);
+   line(40, 70, 60, 70);
+   line(50, 80, 40, 90);
+   line(50, 80, 60, 90);
+}
