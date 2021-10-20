567fe4266fd7f0fc7c9dbb8f4c3ed0979a3ca9e5
MEHovick <ilyastepanov.formal@gmail.com>
2021-10-20 12:43:24 +0500

    House update

--- house.cpp
+++ house.cpp
@@ -11,4 +11,11 @@ void house() {
    setfillstyle(SOLID_FILL, RED);
    setcolor(RED);
    fillpoly(3, shape);
+   setfillstyle(SOLID_FILL, WHITE);
+   rectangle(150, 250, 250, 350);
+   bar(150, 250, 250, 350);
+   setcolor(BLACK);
+   setlinestyle(SOLID_LINE, 1, 8);
+   line(150, 300, 250, 300);
+   line(200, 250, 200, 350);
 }
